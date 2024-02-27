fa45180525e04546631e0d36272c43d6f23d151b
LAB2-PC15 <LAB2-PC15@LAB2-PC15>
2024-02-26 21:48:14 -0300

    aula 4 commit

---
+++ aula_4/src/model/ContaPoupanca.java
@@ -0,0 +1,34 @@
+package model;
+
+public class ContaPoupanca extends Conta {
+  private double rendimento;
+  ContaPoupanca(String email, double rendimento) {
+  super(email);
+  this.rendimento = rendimento;
+  }
+  public String Saque(double valorSaque) {
+	   if(valorSaque <= saldo) {
+	   saldo =- valorSaque;
+	    
+		return (
+				"Valor Sacado: " + valorSaque
+				+ "\nValor Saldo Após o Saque: " 
+				+ saldo
+		);
+	} else {
+		return "Saldo Insuficiente";
+   }
+ }	
+	public String Deposito(double valorDeposito) {
+		saldo += valorDeposito + (valorDeposito*rendimento);
+	    return ("Valor Sacado: " + valorDeposito
+	    		+ "\nValor Saldo Após o Saque: "
+	    		+ saldo);
+	}
+	
+	public double getSaldo() {
+     return saldo; 
+	
+}
+	
+}
